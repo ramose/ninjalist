7d7ed6221a9b232add64d2637b143405332276d2
Leo <leopapilaya@gmail.com>
2023-01-08 16:05:23 +0800

    Add styling

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,31 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
-import Navbar from '../comps/NavBar'
+import Head from "next/head";
+import Image from "next/image";
+import {Inter} from "@next/font/google";
+import styles from "../styles/Home.module.css";
+import Navbar from "../comps/NavBar";
+import Link from "next/link";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({subsets: ["latin"]});
 
 export default function Home() {
-  return (
-
-      <div>
-
-        <h1>Homepage</h1>
-        <p>Exercitation adipisicing et adipisicing dolor dolore est ullamco velit. Consectetur commodo eu minim nulla culpa labore. Est in anim dolore sit amet aliqua sunt dolor incididunt quis sint est fugiat nostrud. Eiusmod ipsum est nostrud amet velit nostrud.</p>
-            <p>Exercitation adipisicing et adipisicing dolor dolore est ullamco velit. Consectetur commodo eu minim nulla culpa labore. Est in anim dolore sit amet aliqua sunt dolor incididunt quis sint est fugiat nostrud. Eiusmod ipsum est nostrud amet velit nostrud.</p>
-      </div>
-
-  )
+    return (
+        <div>
+            <h1 className={styles.title}>Homepage</h1>
+            <p className={styles.text}>
+                Exercitation adipisicing et adipisicing dolor dolore est ullamco
+                velit. Consectetur commodo eu minim nulla culpa labore. Est in
+                anim dolore sit amet aliqua sunt dolor incididunt quis sint est
+                fugiat nostrud. Eiusmod ipsum est nostrud amet velit nostrud.
+            </p>
+            <p className={styles.text}>
+                Exercitation adipisicing et adipisicing dolor dolore est ullamco
+                velit. Consectetur commodo eu minim nulla culpa labore. Est in
+                anim dolore sit amet aliqua sunt dolor incididunt quis sint est
+                fugiat nostrud. Eiusmod ipsum est nostrud amet velit nostrud.
+            </p>
+            <Link href="/ninjas" className={styles.btn}>
+              See Ninjas 
+            </Link>
+        </div>
+    );
 }
